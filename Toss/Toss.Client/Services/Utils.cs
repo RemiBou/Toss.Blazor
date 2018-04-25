205a076d89bdbc1398c67b07e916509fe01c6d10
remi_bourgarel <bourgarel.remi@gmail.com>
2018-04-25 19:49:59 +0200

    change navigation starting form validation / error displaying

---
+++ Toss/Toss.Client/Services/Utils.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Browser.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Toss.Client.Services
+{
+    public static class Utils
+    {
+        public static void ConsoleLog(string message)
+        {
+            RegisteredFunction.Invoke<bool>("log", message);
+        }
+    }
+}
