f6da17a2758c529f31fe88601050029c76fe25cf
Rémi BOURGAREL <bourgarel.remi@gmail.com>
2018-06-26 17:04:41 +0200

    finished integration tests with cosmosdb

--- Toss.E2ETest/Server/Data/TossAzureTableRepositoryTest.cs
+++ Toss.E2ETest/Server/Data/TossAzureTableRepositoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,28 +11,27 @@ using Xunit;
 
 namespace Toss.Tests.Server.Data
 {
-    [Collection("AzureTablecollection")]
     public class TossCosmosDBRepository : IDisposable
     {
+        private readonly DocumentClient _client;
         private readonly TossCosmosRepository _sut;
         private readonly Uri _collectionLink;
-        private readonly AzureTableFixture azureTableFixture;
 
 
 
-        public TossCosmosDBRepository(AzureTableFixture azureCosmoFixture)
+        public TossCosmosDBRepository()
         {
-            this.azureTableFixture = azureCosmoFixture;
-            _sut = new TossCosmosRepository(azureCosmoFixture.Client, AzureTableFixture.UnitTestsDatabaseId);
-            azureCosmoFixture.Init().Wait();
-            _collectionLink = UriFactory.CreateDocumentCollectionUri(azureCosmoFixture.Database.Id, "Toss");
+            var config = new ConfigurationBuilder()
+               .AddJsonFile("client-secrets.json")
+               .Build();
+            _client = new DocumentClient(new Uri(config["CosmosDBEndpoint"]), config["CosmosDBKey"]);
+            _sut = new TossCosmosRepository(_client, "UnitTests");
+            _collectionLink = UriFactory.CreateDocumentCollectionUri("UnitTests", "Toss");
         }
 
         [Fact]
         public async Task last_returns_last_items_from_table_ordered_desc_by_createdon()
         {
-
-
             for (int i = 0; i < 60; i++)
             {
                 var command = new TossCreateCommand() { Content = "lorem #ipsum", CreatedOn = new DateTime(2017, 12, 31).AddDays(-i), UserId = "usernametest" };
@@ -52,7 +52,7 @@ namespace Toss.Tests.Server.Data
             await _sut.Create(command);
 
 
-            var toss = azureTableFixture.Client.CreateDocumentQuery<OneTossEntity>(_collectionLink)
+            var toss = _client.CreateDocumentQuery<OneTossEntity>(_collectionLink)
                 .AsEnumerable()
                 .FirstOrDefault();
             Assert.NotNull(toss);
@@ -65,14 +65,14 @@ namespace Toss.Tests.Server.Data
         [Fact]
         public async Task last_returns_toss_matching_hashtag()
         {
-            
+
             for (int i = 0; i < 3; i++)
             {
                 await _sut.Create(new TossCreateCommand() { Content = "lorem #ipsum #toto num" + i, CreatedOn = DateTimeOffset.Now, UserId = "usernametest" });
 
             }
             await _sut.Create(new TossCreateCommand() { Content = "blabla #ipsum #tutu", CreatedOn = DateTimeOffset.Now, UserId = "usernametest" });
-            
+
 
             var tosses = await _sut.Last(2, "toto");
             Assert.Equal(2, tosses.Count());
@@ -83,7 +83,11 @@ namespace Toss.Tests.Server.Data
 
         public void Dispose()
         {
-            azureTableFixture.Clean().Wait();
+            var dbs = _client.CreateDatabaseQuery().ToList();
+            foreach (var item in dbs)
+            {
+                _client.DeleteDatabaseAsync(item.SelfLink).Wait();
+            }
         }
     }
 }
