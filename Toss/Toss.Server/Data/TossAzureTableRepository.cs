57bc0c5ed0d47ebe49b8550092c52528a034a17f
Rémi BOURGAREL <bourgarel.remi@gmail.com>
2018-06-26 12:11:13 +0200

    wip cosmosdb

--- Toss/Toss.Server/Data/TossAzureTableRepository.cs
+++ Toss/Toss.Server/Data/TossAzureTableRepository.cs
@@ -19,13 +19,33 @@ namespace Toss.Server.Data
         public TossCosmosRepository(DocumentClient documentClient, string databaseId)
         {
             _documentClient = documentClient;
-            _database = new Lazy<Database>(() => _documentClient.CreateDatabaseIfNotExistsAsync(new Database() { Id = databaseId }).Result);
-            _collectionLink = new Lazy<Uri>(() => UriFactory.CreateDocumentCollectionUri(databaseId, collectionId));
+            _database = new Lazy<Task<ResourceResponse<Database>>>(() => 
+                _documentClient.CreateDatabaseIfNotExistsAsync(new Database() { Id = databaseId })
+            );
+            _collection = new Lazy<Task<ResourceResponse<DocumentCollection>>>(async () => 
+                await _documentClient.CreateDocumentCollectionIfNotExistsAsync(
+                    (await  GetDatabase()).SelfLink,
+                    new DocumentCollection(){Id = collectionId }
+                )
+            );
         }
         private readonly DocumentClient _documentClient;
-        private readonly Lazy<Database> _database;
-        private readonly Lazy<Uri> _collectionLink;
+        private readonly Lazy<Task<ResourceResponse<Database>>> _database;
+        private readonly Lazy<Task<ResourceResponse<DocumentCollection>>> _collection;
 
+        private async Task<Database> GetDatabase()
+        {
+            return (await _database.Value).Resource;
+        }
+        private async Task<DocumentCollection> GetCollection()
+        {
+            return (await _collection.Value).Resource;
+        }
+
+         private async Task<Uri> GetCollectionUri()
+        {
+            return new Uri((await GetCollection()).SelfLink);
+        }
         /// <summary>
         /// Return the X last toss created.
         /// If none was created an empty list is returned
@@ -34,7 +54,7 @@ namespace Toss.Server.Data
         /// <returns></returns>
         public async Task<IEnumerable<TossLastQueryItem>> Last(int count, string hashTag)
         {
-            return await GetAllResultsAsync(_documentClient.CreateDocumentQuery<OneTossEntity>(_collectionLink.Value)
+            return await GetAllResultsAsync(_documentClient.CreateDocumentQuery<OneTossEntity>(await GetCollectionUri())
                 .Where(t => t.Content.Contains("#" + hashTag))
                 .OrderByDescending(t => t.CreatedOn)
                 .Take(count)
@@ -62,7 +82,7 @@ namespace Toss.Server.Data
 
             var toss = new OneTossEntity(createCommand.Content, createCommand.UserId, createCommand.CreatedOn);
             var tasks = new List<Task>();
-            tasks.Add(_documentClient.CreateDocumentAsync(_collectionLink.Value, toss));
+            tasks.Add(_documentClient.CreateDocumentAsync(await GetCollectionUri(), toss));
             await Task.WhenAll(tasks);
         }
 
