6c77f6934d097ea25924a13baafa149ba37b3c67
remi_bourgarel <bourgarel.remi@gmail.com>
2018-10-04 22:19:23 +0200

    Upgrade blazor

--- Toss/Toss.Client/Program.cs
+++ Toss/Toss.Client/Program.cs
@@ -1,46 +1,17 @@
-﻿using Microsoft.AspNetCore.Blazor.Browser.Rendering;
-using Microsoft.AspNetCore.Blazor.Browser.Services;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.JSInterop;
-using System.Globalization;
-using System.Linq;
-using Toss.Client.Services;
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
 
 namespace Toss.Client
 {
     public class Program
     {
-        public static BrowserServiceProvider serviceProvider;
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
-            serviceProvider = new BrowserServiceProvider(configure =>
-            {
-                configure.Add(new ServiceDescriptor(
-                    typeof(IHttpApiClientRequestBuilderFactory),
-                    typeof(HttpApiClientRequestBuilderFactory),
-                    ServiceLifetime.Scoped));
-                configure.Add(new ServiceDescriptor(
-                    typeof(IAccountService),
-                    typeof(AccountService),
-                    ServiceLifetime.Scoped));
-                configure.Add(new ServiceDescriptor(
-                     typeof(IBrowserCookieService),
-                     typeof(BrowserCookieService),
-                     ServiceLifetime.Singleton));
-                configure.Add(new ServiceDescriptor(
-                    typeof(II18nService),
-                    typeof(RemoteI18nService),
-                    ServiceLifetime.Singleton));
-                configure.Add(new ServiceDescriptor(
-                    typeof(IModelValidator),
-                    typeof(ModelValidator),
-                    ServiceLifetime.Singleton));
-            });
-
-
-            JSRuntime.Current.InvokeAsync<string[]>("navigatorLanguages")
-                .ContinueWith(t => CultureInfo.DefaultThreadCurrentCulture = t.Result.Select(c => CultureInfo.GetCultureInfo(c)).FirstOrDefault())
-                .ContinueWith(t => new BrowserRenderer(serviceProvider).AddComponent<App>("app")) ;
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
     }
 }
---
+++ Toss/Toss.Client/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Blazor.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Toss.Client.Services;
+
+namespace Toss.Client
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.Add(new ServiceDescriptor(
+                    typeof(IHttpApiClientRequestBuilderFactory),
+                    typeof(HttpApiClientRequestBuilderFactory),
+                    ServiceLifetime.Scoped));
+            services.Add(new ServiceDescriptor(
+                    typeof(IAccountService),
+                    typeof(AccountService),
+                    ServiceLifetime.Scoped));
+            services.Add(new ServiceDescriptor(
+                     typeof(IBrowserCookieService),
+                     typeof(BrowserCookieService),
+                     ServiceLifetime.Singleton));
+            services.Add(new ServiceDescriptor(
+                    typeof(II18nService),
+                    typeof(RemoteI18nService),
+                    ServiceLifetime.Singleton));
+            services.Add(new ServiceDescriptor(
+                typeof(IModelValidator),
+                typeof(ModelValidator),
+                ServiceLifetime.Singleton));
+
+
+        }
+
+        public void Configure(IBlazorApplicationBuilder app)
+        {
+
+
+
+            JSRuntime.Current.InvokeAsync<string[]>("navigatorLanguages")
+                .ContinueWith(t => CultureInfo.DefaultThreadCurrentCulture = t.Result.Select(c => CultureInfo.GetCultureInfo(c)).FirstOrDefault())
+                .ContinueWith(t => app.AddComponent<App>("app"));
+        }
+    }
+}
