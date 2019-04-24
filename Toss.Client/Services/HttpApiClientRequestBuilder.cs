6bc4b815c056dcb038f5264df94b86fda7c22847
remi_bourgarel <bourgarel.remi@gmail.com>
2019-04-24 23:21:44 +0200

    Finish upgrade

--- Toss.Client/Services/HttpApiClientRequestBuilder.cs
+++ Toss.Client/Services/HttpApiClientRequestBuilder.cs
@@ -9,27 +9,29 @@ namespace Toss.Client.Services
 {
     public class HttpApiClientRequestBuilder : IHttpApiClientRequestBuilder
     {
+        private readonly string _relativeUri;
         private readonly string _uri;
-        private readonly IUriHelper uriHelper;
+        private readonly IUriHelper _uriHelper;
         private readonly HttpClient _httpClient;
         private Func<HttpResponseMessage, Task> _onBadRequest;
         private Func<HttpResponseMessage, Task> _onOK;
-        private readonly IBrowserCookieService browserCookieService;
-        private readonly IMessageService messageService;
-        public IJsInterop JsInterop { get; }
+        private readonly IBrowserCookieService _browserCookieService;
+        private readonly IMessageService _messageService;
+        private readonly IJsInterop _jsInterop;
 
         public HttpApiClientRequestBuilder(HttpClient httpClient,
             string uri,
             IUriHelper uriHelper,
             IBrowserCookieService browserCookieService, IJsInterop jsInterop, IMessageService messageService)
         {
-            _uri = uri;
-            this.uriHelper = uriHelper;
+            _relativeUri = uri;
+            _uri = uriHelper.ToAbsoluteUri( uri).ToString();
+            _uriHelper = uriHelper;
             _httpClient = httpClient;
-
-            this.browserCookieService = browserCookieService;
-            JsInterop = jsInterop;
-            this.messageService = messageService;
+           
+            _browserCookieService = browserCookieService;
+            _jsInterop = jsInterop;
+            _messageService = messageService;
         }
 
         public async Task Post(byte[] data)
@@ -59,7 +61,7 @@ namespace Toss.Client.Services
         {
             if (data is NotARobot)
             {
-                (data as NotARobot).Token = await JsInterop.Captcha(_uri);
+                (data as NotARobot).Token = await _jsInterop.Captcha(_relativeUri);
             }
         }
 
@@ -83,14 +85,14 @@ namespace Toss.Client.Services
                 case System.Net.HttpStatusCode.Forbidden:
                     break;
                 case System.Net.HttpStatusCode.InternalServerError:
-                    messageService.Error("A server error occured, sorry");
+                    _messageService.Error("A server error occured, sorry");
                     break;
                     //other case , we do nothing, I'll add this case as needed
             }
         }
         private async Task<HttpRequestMessage> PrepareMessageAsync(HttpRequestMessage httpRequestMessage)
         {
-            string csrfCookieValue = await browserCookieService.Get(c => c.Equals("CSRF-TOKEN"));
+            string csrfCookieValue = await _browserCookieService.Get(c => c.Equals("CSRF-TOKEN"));
             if (csrfCookieValue != null)
                 httpRequestMessage.Headers.Add("X-CSRF-TOKEN", csrfCookieValue);
             return httpRequestMessage;
@@ -101,7 +103,7 @@ namespace Toss.Client.Services
         }
         private async Task ExecuteHttpQuery(Func<Task<HttpResponseMessage>> httpCall)
         {
-            messageService.Loading();
+            _messageService.Loading();
             HttpResponseMessage response = null;
             try
             {
@@ -111,12 +113,12 @@ namespace Toss.Client.Services
                 }
                 catch
                 {
-                    messageService.Error("Connection error, server is down or you are not connected to the same network (internet).");
+                    _messageService.Error("Connection error, server is down or you are not connected to the same network (internet).");
                     throw;
                 }
                 finally
                 {
-                    messageService.LoadingDone();
+                    _messageService.LoadingDone();
                 }
                 await HandleHttpResponse(response);
             }
@@ -182,9 +184,9 @@ namespace Toss.Client.Services
             OnOK(() =>
            {
                if (!string.IsNullOrEmpty(successMessage))
-                   messageService.Info(successMessage);
+                   _messageService.Info(successMessage);
                if (!string.IsNullOrEmpty(navigateTo))
-                   uriHelper.NavigateTo(navigateTo);
+                   _uriHelper.NavigateTo(navigateTo);
            });
             return this;
         }
