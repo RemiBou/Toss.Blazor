86bed0c28faa4cf034703d916c15cf9769df19f6
remi_bourgarel <bourgarel.remi@gmail.com>
2019-05-06 23:41:09 +0200

    WIP finish backend impl of connected tags

--- Toss.Server/Data/Indexes/Toss_ConnectedTags.cs
+++ Toss.Server/Data/Indexes/Toss_ConnectedTags.cs
@@ -3,27 +3,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Toss.Server.Models;
 
 namespace Toss.Server.Data.Indexes
 {
-    public class Toss_ConnectedTags :  AbstractIndexCreationTask<TossEntity, TossConnectedTagsIndex>
+    public class Toss_ConnectedTags : AbstractMultiMapIndexCreationTask<TossConnectedTagsIndex>
     {
         public Toss_ConnectedTags()
         {
-            Map = tosses => from toss in tosses
-                            from tag in toss.Tags
+            AddMap< TossEntity>(tosses => from toss in tosses
+                            from tag in toss.Tags.Distinct()
+                            from tag2 in toss.Tags.Distinct()
+                            where tag != tag2
                             select new TossConnectedTagsIndex()
                             {
-                                Tag = tag,
-                                ConnectedTags = toss.Tags.Where(t => t != tag)
-                            };
+                                Tag1 = tag,
+                                Tag2 = tag2,
+                                Count = 1
+                            });
+            AddMap<ApplicationUser>(users => from user in users
+                                         from tag in user.Hashtags.Distinct()
+                                         from tag2 in user.Hashtags.Distinct()
+                                         where tag != tag2
+                                         select new TossConnectedTagsIndex()
+                                         {
+                                             Tag1 = tag,
+                                             Tag2 = tag2,
+                                             Count = 1
+                                         });
             Reduce = results => from result in results
-                                group result by result.Tag
+                                group result by new { result.Tag1, result.Tag2 }
                                 into g
                                 select new TossConnectedTagsIndex()
                                 {
-                                    Tag = g.Key,
-                                    ConnectedTags = g.SelectMany(t => t.ConnectedTags).Distinct()
+                                    Tag1 = g.Key.Tag1,
+                                    Tag2 = g.Key.Tag2,
+                                    Count = g.Sum(c => c.Count)
+                                    
                                 };
         }
     }
